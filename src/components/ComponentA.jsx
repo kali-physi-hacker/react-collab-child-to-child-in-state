477380b8b62311070039a5f56d89ca2b08a51d99
Desmond Brown <browndesmond30@yahoo.com>
2020-10-08 18:31:54 +0000

    Kali: Initialized Repo

--- src/components/ComponentA.jsx
+++ src/components/ComponentA.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ComponentA = props => {
     return (
-        <div style={{marginTop: 120, backgroundColor: "#343276", width: "100%", height: 200, color: "white", padding: 80}}>
+        <div style={{marginTop: 120, backgroundColor: "#343276", width: "100%", height: 200, color: "white", paddingTop: 80}}>
             <h1>State from Component B</h1>
             <h3>Cart: {props.cart}</h3>
         </div>
